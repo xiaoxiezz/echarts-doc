c44fac07228059afa896d0dfe6affa9da3f950f1
iMaeGoo <mail1st@qq.com>
2021-11-23 19:18:32 +0800

    Update env.asf.js

--- config/env.asf.js
+++ config/env.asf.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    galleryViewPath: 'https://echarts.apache.org/examples/${lang}/view.html?c=',
-    galleryEditorPath: 'https://echarts.apache.org/examples/${lang}/editor.html?c=',
-    handbookPath: 'https://echarts.apache.org/handbook/${lang}/',
-    websitePath: 'https://echarts.apache.org',
+    galleryViewPath: '/examples/${lang}/view.html?c=',
+    galleryEditorPath: '/examples/${lang}/editor.html?c=',
+    handbookPath: '/handbook/${lang}/',
+    websitePath: '/',
 
     imagePath: 'asset/img/',
     gl: {
